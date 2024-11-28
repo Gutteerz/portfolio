088be37f139c77ce6a6af9c687ab1c3dc46a24d5
Storm Selvig <storms@uia.no>
2024-11-28 23:19:12 +0100

    minor imporvments

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.10-slim
 WORKDIR /app
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
-CMD ["flask", "run", "--host=0.0.0.0"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "run:app"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 def create_app():
+
+    load_dotenv()
+
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a strong secret key
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
     # Import and register routes
     from .routes import main
